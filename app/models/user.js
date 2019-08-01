d88e653c5372eb950d52a9c3a8abac7438f5a63b
Hoang Thanh <thanh.hoang@luxstay.com>
2019-08-01 09:01:09 +0000

    update base

--- app/models/user.js
+++ app/models/user.js
@@ -1,105 +1,34 @@
 'use strict';
 
-const bcrypt = require('bcrypt');
-const createGuts = require('../helpers/model-guts.js');
-const { AuthenticationError } = require('../helpers/errors');
-const { TokenHandler } = require('../helpers/jwt-token');
-const name = 'User';
-const tableName = 'users';
+const { Model } = require('objection');
+const CustomQueryBuilder = require('../helpers/custom_query_builder');
+const knex = require('../../config/database');
 
-// Properties that are allowed to be selected from the database for reading.
-// (e.g., `password` is not included and thus cannot be selected)
-const selectableProps = [
-    'id',
-    'email',
-    'first_name',
-    'last_name'
-];
+class User extends Model {
+    static get tableName() {
+        return 'users';
+    }
 
-// Bcrypt functions used for hashing password and later verifying it.
-const SALT_ROUNDS = 10;
-const encryptPassword = password => bcrypt.hash(password, SALT_ROUNDS);
-const verifyPassword = (password, hash) => bcrypt.compare(password, hash);
+    static get softDelete() {
+        return true;
+    }
 
-const beforeSave = user => {
-    if (!user.password) return Promise.resolve(user);
+    static get maxPageSize() {
+        return 100;
+    }
 
-    return encryptPassword(user.password)
-        .then(hash => ({
-            ...user,
-            password: hash
-        }))
-        .catch(err => `Error hashing password: ${ err }`);
-};
+    static get selectableProps() {
+        return ['id'];
+    }
 
-module.exports = knex => {
-    const guts = createGuts({
-        knex,
-        name,
-        tableName,
-        selectableProps
-    });
+    static get deletedColumn() {
+        return 'deleted_at';
+    }
 
-    const create = props => beforeSave(props)
-        .then(user => guts.create(user));
+    static get QueryBuilder() {
+        return CustomQueryBuilder;
+    }
+}
+User.knex(knex);
 
-    const login = (ctx) => {
-        const matchErrorMsg = 'Email or password do not match';
-        const props = ['id', 'first_name', 'last_name', 'email', 'password'];
-
-        return knex.select(props)
-            .from(tableName)
-            .where({ email: ctx.email })
-            .whereNull('deleted_at')
-            .first()
-            .then(user => {
-                if (!user) return Promise.reject(new AuthenticationError('Email or password is invalid!', 401, '', []));
-                return user;
-            })
-            .then((user) => Promise.all([user, verifyPassword(ctx.password, user.password)]))
-            .then(([user, isMatch]) => {
-                if (!isMatch) return Promise.reject(new AuthenticationError(matchErrorMsg, 401, '', []));
-                // Create token
-                let token = TokenHandler.generateJWT(user);
-                // Return data
-                return {
-                    user: {
-                        id: user.id,
-                        first_name: user.first_name,
-                        last_name: user.last_name,
-                        email: user.email
-                    },
-                    token
-                };
-            });
-    };
-    const validateToken = (token) => {
-        return TokenHandler.validateToken(token);
-    };
-    const hashPassword = (password) => {
-        return encryptPassword(password);
-    };
-    const checkPassword = async (id, password) => {
-        const user = await knex.select(['password'])
-            .from(tableName)
-            .where({ id })
-            .whereNull('deleted_at')
-            .first();
-        if (user) {
-            const verify = await verifyPassword(password, user.password);
-            if (verify) return true;
-
-            return false;
-        }
-        return false;
-    };
-    return {
-        ...guts,
-        create,
-        login,
-        validateToken,
-        hashPassword,
-        checkPassword
-
-    };
-};
+module.exports = User;
--- services/base.service.js
+++ services/base.service.js
@@ -2,7 +2,7 @@
 
 const { MoleculerClientError, ValidationError } = require('moleculer').Errors;
 const { AuthenticationError } = require('../app/helpers/errors');
-const { User, Jwt } = require('../app/models');
+const User = require('../app/models/user');
 
 module.exports = {
     name: 'base',
@@ -10,18 +10,6 @@ module.exports = {
      * Service settings
      */
     settings: {
-    /** Secret for JWT */
-        JWT_SECRET: process.env.JWT_SECRET || 'jwt-conduit-secret',
-
-        /** Public fields */
-        fields: ['_id', 'email', 'password'],
-
-        /** Validator schema for entity */
-        entityValidator: {
-            password: { type: 'string', min: 6 },
-            email: { type: 'email' }
-        },
-        maxRetryTime: 3
     },
 
     /**
@@ -42,66 +30,21 @@ module.exports = {
          *
          * @returns {Object} Logged in user with token
          */
-        login: {
+        getUser: {
             params: {
-                user: {
-                    type: 'object',
-                    props: {
-                        email: { type: 'email' },
-                        password: { type: 'string', min: 8 },
-                        $$strict: true
-                    }
-                }
+                id: { type: 'number', convert: true, integer: true },
+                $$strict: true
             },
             handler(ctx) {
-                return User.login(ctx.params.user)
+                return User.query().findOne('id', ctx.params.id)
                     .then(data => {
-                        return Promise.all([data, Jwt.create(data)]);
-                    })
-                    .then(data => {
-                        return this.transformLogin(data[0]);
-                    })
-                    .catch(err => {
-                        if (err instanceof AuthenticationError) return Promise.reject(err);
-                        return Promise.reject(new MoleculerClientError(err.message, 500, '', [{ message: err.message }]));
-                    });
-            }
-        },
-
-        register: {
-            params: {
-                user: {
-                    type: 'object',
-                    props: {
-                        email: { type: 'email', mode: 'precise' },
-                        first_name: { type: 'string', min: 1, max: 200 },
-                        last_name: { type: 'string', min: 1, max: 200 },
-                        password: { type: 'string', min: 8, pattern: /^[ -~]*$/ },
-                        password_confirmation: { type: 'string', min: 8, pattern: /^[ -~]*$/ },
-                        $$strict: true
-                    }
-                }
-            },
-            handler(ctx) {
-                const params = ctx.params.user;
-                return User.findOne({ email: params.email })
-                    .then(user => {
-                        if (user) throw new ValidationError('user-existed', null, '', []);
-
-                        if (params.password !== params.password_confirmation) {
-                            throw new ValidationError('password-confirmation-not-match', null, '', []);
-                        }
-
-                        delete params.password_confirmation;
-                        // Emit an event to all service
-                        this.broker.emit('base.register', { service: 'base', method: 'register', params });
-                        return User.create(params);
-                    }).then(result => {
-                        if (result !== undefined) return { data: result };
-                        throw new MoleculerClientError('Unexpected Error', null, '', []);
+                        return {
+                            data
+                        };
                     });
             }
         }
+
 
     },
     /**
@@ -133,19 +76,6 @@ module.exports = {
      * Methods
      */
     methods: {
-        transformLogin(data) {
-            if (data.user && data.token) {
-                return Promise.resolve({
-                    data: {
-                        token_type: 'Bearer',
-                        access_token: data.token.accessToken,
-                        refresh_token: data.token.refreshToken,
-                        user: data.user
-                    }
-                });
-            }
-            return Promise.reject(new MoleculerClientError('Unexpected Error!', 500, '', []));
-        }
     },
 
     /**
